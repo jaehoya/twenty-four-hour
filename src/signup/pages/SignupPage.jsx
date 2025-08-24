e2565e70de4e62194fc4f0e4ff27be593ab72106
jinbaaaaaang <jinbaaaaang@gmail.com>
2025-08-24 12:08:02 +0900

    feat: SignupPage 추가 및 배경/로고 이미지 임포트

---
+++ src/signup/pages/SignupPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SignupForm from "../components/SignupForm";
+import Bg from "../../assets/signup/background_gradient.png";
+import Logo from "../../assets/signup/24_logo.png";
+
+function SignupPage() {
+    return (
+        <>
+        </>
+    )
+}
+
+export default SignupPage;
