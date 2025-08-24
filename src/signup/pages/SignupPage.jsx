0ce7806d8c95b47df0af0f30957ce5c01c077e36
jinbaaaaaang <jinbaaaaang@gmail.com>
2025-08-24 12:09:04 +0900

    feat: SignupPage 레이아웃 구현(배경·로고 적용, 폼 배치)

--- src/signup/pages/SignupPage.jsx
+++ src/signup/pages/SignupPage.jsx
@@ -5,8 +5,14 @@ import Logo from "../../assets/signup/24_logo.png";
 
 function SignupPage() {
     return (
-        <>
-        </>
+        <div className="flex flex-row w-full min-h-screen">
+        <img src={Bg} className="m-[17px] h-auto max-h-screen w-[50%]"/>
+        <div className="flex flex-col justify-center items-center w-full min-h-screen">
+            <img src={Logo} className="w-[82px] h-[65px] mb-[30px]" />
+            <span className="text-[21px] font-semibold mb-[60px]">TFH Storage 로그인</span>
+            <SignupForm />
+        </div>
+        </div>
     )
 }
 
