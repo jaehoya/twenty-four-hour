450106c8c9bd6d17446daecab54148fbcae93b86
jinbaaaaaang <jinbaaaaang@gmail.com>
2025-08-24 12:11:00 +0900

    feat: App에 회원가입 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import './App.css'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignupPage from './signup/pages/SignupPage';
 function App() {
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path='/signup' element={<SignupPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
