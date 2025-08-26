16e839e8ca6f8865aea9f058f5628b19ef6a2d65
yuji4 <165403706+yuji4@users.noreply.github.com>
2025-08-26 15:42:29 +0900

    Revert "feat. LogOut and DeleteUser 기능 추가"

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,19 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { signup, login, logout, deleteUser } = require("../controllers/user.controller");
+const { signup, login } = require("../controllers/user.controller");
 const { signupValidator, loginValidator } = require("../middlewares/validators/user.dto");
-const authMiddleware = require("../middlewares/auth.middleware");
 
 // POST /api/users/signup → 회원가입
 router.post("/signup", signupValidator, signup);
 
 // POST /api/users/login -> 로그인
 router.post("/login", loginValidator, login);
-
-// POST /api/users/logout -> 로그아웃
-router.post("/logout", authMiddleware, logout);
-
-// DELETE /api/users -> 회원 탈퇴
-router.delete("/", authMiddleware, deleteUser);
 
 module.exports = router;
