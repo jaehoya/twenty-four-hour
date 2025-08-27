aa85c1f50495bf5a2cf9c3a4b5510e534b3add8d
YUJI\jungy <yuyuyu2635@gmail.com>
2025-08-28 01:25:36 +0900

    feat: 파일 업로드 라우트에 JWT 인증 미들웨어 적용

--- src/routes/file.routes.js
+++ src/routes/file.routes.js
@@ -2,8 +2,14 @@ const express = require("express");
 const router = express.Router();
 const upload = require("../middlewares/upload");
 const { uploadFile } = require("../controllers/file.controller");
+const authenticateToken = require("../middlewares/auth");
 
 // POST /api/files/upload -> 파일 업로드 (필드명: file)
-router.post("/upload", upload.single("file"), uploadFile);
+router.post(
+    "/upload",
+    authenticateToken,
+    upload.single("file"),
+    uploadFile
+);
 
 module.exports = router;
