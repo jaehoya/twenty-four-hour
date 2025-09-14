ceca2a54b875dda7272dfd9ffa3868b710d08014
jinbaaaaaang <jinbaaaaang@gmail.com>
2025-09-14 17:42:25 +0900

    feat: 업로드 페이지 신규 개발
    - 파일/폴더 관리 UI 완전 신규 구현
    - 반응형 그리드 및 선택 기능 개발
    - 모바일 플로팅 버튼 및 드롭다운 메뉴 신규 개발
    - 전체 컴포넌트 및 레이아웃 신규 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
+import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SignupPage from './signup/pages/SignupPage';
 import Upload from './upload/pages/Upload';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<SignupPage />} />
         <Route path='/upload' element={<Upload />} />
       </Routes>
     </Router>
@@ -14,3 +13,4 @@ function App() {
 }
 
 export default App;
+
