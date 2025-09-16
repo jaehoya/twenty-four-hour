09f566587a74f8e8fd03d0716322f320f7b94012
jinbaaaaaang <jinbaaaaang@gmail.com>
2025-09-16 10:59:40 +0900

    fix: upload 페이지 SearchBar 컴포넌트 수정

--- src/upload/components/content/SearchBar.jsx
+++ src/upload/components/content/SearchBar.jsx
@@ -5,7 +5,7 @@ function SearchBar() {
     return (
         <div className="w-full h-full flex flex-row items-center justify-center border-[1px] border-[#BDD0E6] p-4 rounded-[30px] bg-white md:bg-[#F3F5F9]">
             <img src={SearchIcon} alt="search" className="w-4 h-4 md:w-6 md:h-6 m-2"/>
-            <input type="text" placeholder="검색" className="w-full text-[1rem] text-[#989AA9] focus:outline-none focus:border-none"/>
+            <input type="text" placeholder="검색" className="w-full text-[1rem] text-[#989AA9] md:bg-[#F3F5F9] focus:outline-none focus:border-none"/>
         </div>
     )
 }
