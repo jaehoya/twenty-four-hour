f337a30d2863de9eb9e185b516bdb1b709d9c1bc
heehee8878 <kkm601266@gmail.com>
2025-09-19 00:11:46 +0900

    fix: tailwind upgrade - version 4

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [react(), svgr({
@@ -8,5 +9,6 @@ export default defineConfig({
             svgo: true,
             svgoConfig: { plugins: [{ name: "prefixIds" }] },
         },
-    })],
+    }),
+    tailwindcss(),],
 });
