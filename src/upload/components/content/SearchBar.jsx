bd9a8a22f9119af4992e48b8066e111a05f4f209
heehee8878 <kkm601266@gmail.com>
2025-09-19 01:36:49 +0900

    fix: upload page - UI proportion update

--- src/upload/components/content/SearchBar.jsx
+++ src/upload/components/content/SearchBar.jsx
@@ -4,8 +4,8 @@ import SearchIcon from "../../../assets/upload/search_icon.svg";
 function SearchBar() {
     return (
         <div className="w-full h-full flex flex-row items-center justify-center border-[1px] border-[#BDD0E6] p-4 rounded-[30px] bg-white md:bg-[#F3F5F9]">
-            <img src={SearchIcon} alt="search" className="w-4 h-4 md:w-6 md:h-6 m-2"/>
-            <input type="text" placeholder="검색" className="w-full text-[1rem] text-[#989AA9] md:bg-[#F3F5F9] focus:outline-none focus:border-none"/>
+            <img src={SearchIcon} alt="search" className="w-4 h-4 md:w-4.5 md:h-4.5 mr-3"/>
+            <input type="text" placeholder="검색" className="w-full text-[10pt] text-[#989AA9] md:bg-[#F3F5F9] focus:outline-none focus:border-none"/>
         </div>
     )
 }
--- src/upload/components/layout/Usage.jsx
+++ src/upload/components/layout/Usage.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function Usage() {
     return (
-        <div className="hide md:block md:w-[14.68svw] md:h-[7.87svh] bg-white border-[1px] border-[#DAE0E9] rounded-[10px] md:ml-3 p-4 flex flex-col justify-center">
+        <div className="hide md:mr-3 md:mb-3 md:block md:w-auto md:h-18 bg-white border-[1px] border-[#DAE0E9] rounded-[10px] md:ml-3 p-4 flex flex-col justify-center">
             <div className="flex justify-between items-center mb-1">
-                <span className="text-[#2A2D41] font-medium text-[0.9375rem]">사용량</span>
-                <span className="text-[#989AA9] text-[0.9375rem]">0GB / 2GB</span>
+                <span className="text-[#2A2D41] font-medium text-[9pt]">사용량</span>
+                <span className="text-[#989AA9] text-[9pt]">0GB / 2GB</span>
             </div>
             
-            <div className="w-full h-2 bg-[#E5E7EB] rounded-full overflow-hidden">
+            <div className="w-full h-1.5 bg-[#E5E7EB] rounded-full overflow-hidden">
                 <div className="h-full bg-gradient-to-r from-[#0D4CFF] to-[#33AAFF] rounded-full w-0"></div>
             </div>
         </div>
