987738af759a7c1c65ec3cc70002c5338cc528b4
heehee8878 <kkm601266@gmail.com>
2025-10-02 19:58:44 +0900

    feat: Add Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignupPage from './signup/pages/SignupPage';
+import LoginPage from "./login/pages/LoginPage";
 import Upload from './upload/pages/Upload';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/signup' element={<SignupPage />} />
+        <Route path='/login' element={<LoginPage />} />
         <Route path='/upload' element={<Upload />} />
       </Routes>
     </Router>
