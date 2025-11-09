116ea962574b44e8caba0e698c3458e9d2fe76b1
jinbaaaaaang <jinbaaaaang@gmail.com>
2025-11-09 15:13:49 +0900

    feat: develop-frontend 기반으로 파일 업로드 기능 추가
    - develop-frontend 브랜치를 기반으로 feature/file-upload-frontend-only의 변경사항 적용
    - Upload 페이지 레이아웃 컴포넌트로 리팩토링 및 중첩 라우팅 구현
    - 드래그앤드롭 파일 업로드 기능
    - 파일 다운로드 및 삭제 기능
    - 한글 파일명 지원
    - 프론트엔드 독립 실행을 위한 모킹 모드

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import SignupPage from './signup/pages/SignupPage';
 import LoginPage from "./login/pages/LoginPage";
 import Upload from './upload/pages/Upload';
+import StoragePage from './upload/pages/StoragePage';
+import FavoritesPage from './upload/pages/FavoritesPage';
+import TrashPage from './upload/pages/TrashPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Upload />} />
         <Route path='/signup' element={<SignupPage />} />
         <Route path='/login' element={<LoginPage />} />
-        <Route path='/upload' element={<Upload />} />
+        <Route path='/upload' element={<Upload />}>
+          <Route index element={<StoragePage />} />
+          <Route path='favorites' element={<FavoritesPage />} />
+          <Route path='trash' element={<TrashPage />} />
+        </Route>
+        <Route path='/' element={<Navigate to="/upload" replace />} />
       </Routes>
     </Router>
   );
