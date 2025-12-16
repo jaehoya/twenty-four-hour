6c15480497f299df717b7ab8feff3dd707174d24
YUJI\jungy <yuyuyu2635@gmail.com>
2025-12-17 02:38:12 +0900

    fix: 토큰이 고정돼서 꼬이던 문제를 요청마다 토큰을 동적으로 붙이는 코드로 변경하여 해결

--- fe/src/utils/api.js
+++ fe/src/utils/api.js
@@ -1,11 +1,19 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_ENDPOINT || 'http://localhost:4000/api',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
-    },
+  baseURL: import.meta.env.VITE_API_ENDPOINT || 'http://localhost:4000/api',
 });
+
+// 요청마다 토큰을 동적으로 붙이기
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
