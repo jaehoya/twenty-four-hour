5c8e90869b8bd7f754c59b4f81b3cd8be48a0e5a
jaehoya <hmujaeho@gmail.com>
2025-12-16 19:23:45 +0900

    remove all files

--- backend/src/routes/disk.routes.js
+++
@@ -1,8 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const diskController = require('../controllers/disk.controller');
-
-router.get('/usage', diskController.getDiskUsage);
-
-module.exports = router;
