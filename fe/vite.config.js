45fbc6ad12ad01f4742e2e4315320b9d9323adb7
YUJI\jungy <yuyuyu2635@gmail.com>
2025-12-17 02:39:22 +0900

    chore: 안 쓰는 be 폴더 삭제

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     ],
     server: {
         '/api': {
-            target: 'http://tfh.kro.kr',
+            target: 'http://localhost:4000',
             changeOrigin: true,
         },
     },
